702ec3cd3ca72a0556f312984c3910e65893d2ad
Nikolos Black <Nikolosblack@gmail.com>
2022-02-19 00:25:09 +0200

    add tasks result 1-8

---
+++ hw-1/js/task-2-8.js
@@ -0,0 +1,92 @@
+// task2
+
+const myLastName = "My last name is Chympaiesh";
+console.log(myLastName);
+
+// task3
+
+// let myNameInUkrainian;
+// let myNameInEnglish;
+
+// myNameInUkrainian = "Микола";
+// myNameInEnglish = "Nicholas";
+
+// alert(myNameInUkrainian);
+// alert(myNameInEnglish);
+
+// myNameInUkrainian = myNameInEnglish;
+
+// alert(myNameInUkrainian);
+// alert(myNameInEnglish);
+
+// task4
+
+const user = {
+    name: "Nicholas",
+    age: 27,
+    likesCat: true,
+    hasFavoriteFood: undefined,
+    favoriteVegetable: null
+}
+console.log(
+    typeof user.name + " / " +
+    typeof user.age + " / " +
+    typeof user.likesCat + " / " +
+    typeof user.hasFavoriteFood + " / " +
+    typeof user.favoriteVegetable
+);
+
+// task5
+
+const isAdult = confirm("Are you an adult?");
+console.log(isAdult);
+
+// task6
+
+const myName = "Nicholas";          // String
+const myAge = 27;                   // Number
+const myGroup = "Lv-678.JS Core";   // String
+const yearOfBirth = 1994;           // Number
+const isMarried = false;            // Baloon
+
+console.log(myAge);
+console.log(yearOfBirth);
+console.log(isMarried);
+console.log(myName);
+console.log(myGroup);
+
+const hasChild = null;
+const hasCat = undefined;
+
+console.log(hasChild);
+console.log(hasCat);
+
+// task7
+
+const userLogin = prompt("Please enter your Login");
+const userEmail = prompt("Please enter your Email");
+const userPassword = prompt("Please enter your Password");
+alert("Dear " + userLogin + " , " + " your email is " + userEmail + " , " + " your password is " + userPassword + "✌️");
+document.querySelector(".prompt__result").innerHTML = "Dear " + userLogin + " , " + " your email is " + userEmail + " , " + " your password is " + userPassword + "✌️";  
+
+// task8
+
+const secondInAMinute = 60;
+const minuteInAnHour = 60;
+const secondsInAnHour = secondInAMinute * minuteInAnHour;
+alert(secondsInAnHour); 
+
+const hoursInADay = 24;
+const secondsInADay = secondsInAnHour * hoursInADay;
+alert(secondsInADay); 
+
+const daysInAMonth = 28; //February
+const secondsInAMonth = secondsInADay * daysInAMonth;
+alert(secondsInAMonth); 
+
+document.querySelector(".result__seconds").innerHTML = "There are " + secondsInAnHour  + " seconds in an hour⏰ " +  " and " + secondsInADay + " seconds in a day⌚. " + " In 28 days it’s " + secondsInAMonth + " seconds⌛.";
+
+
+
+
+
